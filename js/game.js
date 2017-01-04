e50f1c475e02c614335edb3d9c5ae1b46564e421
Craig Morton <CraigMorton22@gmail.com>
2017-01-04 15:24:52 +0000

    Prevent player continuing gameplay on game over screen (#11)

--- js/game.js
+++ js/game.js
@@ -79,6 +79,7 @@
       }
 
       function checkKey(e) {
+        if (finished) return;
         e = e || window.event;
         activeEntry.push(e.keyCode);
         $gameEntry.innerHTML += e.key;
@@ -116,6 +117,7 @@
   // Start Game and counter
 
   let started = false;
+  let finished = false;
 
   window.onkeydown = function() {
     if(!started) {
@@ -127,6 +129,7 @@
       var countdownTimer = setInterval(function() {
         counter--;
         if(counter <= 0) {
+          finished = true;
           document.body.classList += 'game-over';
           window.clearInterval(countdownTimer);
 
